38ead9b5d3ccdaf2cd2e2a065a580a721135a202
huangguoqiao <huangguoqiao@xhqb.com>
2016-04-14 16:33:24 +0800

    Initial Commit

---
+++ AFNetworking3.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking3.0
+//
+//  Created by shizi on 16/4/14.
+//  Copyright © 2016年 黄小黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworking3.0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AFNetworking3.0
+//
+//  Created by shizi on 16/4/14.
+//  Copyright © 2016年 黄小黄. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
